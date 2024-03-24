55c9090c72c80d609ce49e0c7d8b396ac0b382d5
avarelop <alvaro.valera@samara.energy>
2024-03-24 19:04:54 +0100

    feat: solution for hamming problem.

---
+++ exercism/path_c/hamming/hamming_2.c
@@ -0,0 +1,13 @@
+#include "hamming.h"
+#include <string.h>
+
+int compute(const char *lhs, const char *rhs){
+    if(strlen(lhs) != strlen(rhs) || !lhs || !rhs){return -1;}
+
+    int diff = 0;
+    for (; *lhs && *rhs; lhs++, rhs++) {
+        if (*lhs != *rhs) { diff++; }
+    }
+
+    return diff;
+}
